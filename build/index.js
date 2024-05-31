ad97e058eaf48c9b855f4b3df1c9c38c788a363a
kadima <williamsvgw@gmail.com>
2024-05-31 13:48:52 +0100

    iterators,iterable,iterableiterators

--- build/index.js
+++ build/index.js
@@ -1,8 +1,80 @@
 "use strict";
-var func = [];
-for (var i = 0; i < 3; i++) {
-    func.push(function () {
-        console.log(i);
-    });
+class Component {
+    constructor(name) {
+        this.name = name;
+    }
 }
-console.log(func[0]());
+// ITERATOR
+class Frame {
+    constructor(name, components) {
+        this.name = name;
+        this.components = components;
+        this.pointer = 0;
+    }
+    next() {
+        if (this.pointer < this.components.length) {
+            return {
+                done: false,
+                value: this.components[this.pointer++]
+            };
+        }
+        else {
+            return {
+                done: true,
+                value: null
+            };
+        }
+    }
+}
+//ITERABLE
+class FrameIterable {
+    constructor(name, components) {
+        this.name = name;
+        this.components = components;
+    }
+    [Symbol.iterator]() {
+        let pointer = 0;
+        let components = this.components;
+        return {
+            next() {
+                if (pointer < components.length) {
+                    return {
+                        done: false,
+                        value: components[pointer++]
+                    };
+                }
+                else {
+                    return {
+                        done: true,
+                        value: null
+                    };
+                }
+            }
+        };
+    }
+}
+//ITERABLEITERATOR
+class FrameIterableIterator {
+    constructor(name, components) {
+        this.name = name;
+        this.components = components;
+        this.pointer = 0;
+    }
+    next() {
+        if (this.pointer < this.components.length) {
+            return {
+                done: false,
+                value: this.components[this.pointer++]
+            };
+        }
+        else {
+            return {
+                done: true,
+                value: null
+            };
+        }
+    }
+    [Symbol.iterator]() {
+        return this;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,83 @@
-var func = []
-
-for (var i=0; i < 3; i++){
-
-    func.push(function(){
-        console.log(i)
-    })
+class Component{
+    constructor(public name:string){}
 }
 
-console.log(func[0]())
+// ITERATOR
+
+class Frame implements Iterator<Component>{
+
+    private pointer = 0
+
+    constructor(public name:string, public components:Component[]){}
+
+    public next(): IteratorResult<Component> {
+        if(this.pointer < this.components.length){
+
+            return {
+                done:false,
+                value:this.components[this.pointer++]
+            }
+        }else{
+            return {
+                done:true,
+                value:null
+            }
+        }
+    }
+}
+
+//ITERABLE
+class FrameIterable implements Iterable<Component>{
+
+    constructor(public name:string, public components:Component[]){}
+
+    [Symbol.iterator](){
+        let pointer = 0;
+        let components = this.components
+
+        return {
+            next(): IteratorResult<Component> {
+                if(pointer <components.length){
+        
+                    return {
+                        done:false,
+                        value:components[pointer++]
+                    }
+                }else{
+                    return {
+                        done:true,
+                        value:null
+                    }
+                }
+            }
+        }
+    }
+}
+
+//ITERABLEITERATOR
+
+class FrameIterableIterator implements IterableIterator<Component>{
+
+    private pointer = 0
+
+    constructor(public name:string, public components:Component[]){}
+
+    public next(): IteratorResult<Component> {
+        if(this.pointer < this.components.length){
+
+            return {
+                done:false,
+                value:this.components[this.pointer++]
+            }
+        }else{
+            return {
+                done:true,
+                value:null
+            }
+        }
+    }
+
+    [Symbol.iterator]():IterableIterator<Component>{
+        return this
+    }
+}
